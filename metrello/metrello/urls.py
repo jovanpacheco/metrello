e866fe51ff933e08fdc81744eb9a4cd2239a1ada
Jovan Pacheco <jovanjpacheco@gmail.com>
2018-10-24 14:32:37 -0400

    Login url

--- metrello/metrello/urls.py
+++ metrello/metrello/urls.py
@@ -32,5 +32,6 @@ urlpatterns = [
     path('auth/refresh_token/', refresh_jwt_token),
     path('auth/verify_jwt_token/', verify_jwt_token),
     path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'favicon.ico')),
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('login', TemplateView.as_view(template_name='login.html'), name="login"),
+    path('', TemplateView.as_view(template_name='index.html'), name="inicio"),
 ]
