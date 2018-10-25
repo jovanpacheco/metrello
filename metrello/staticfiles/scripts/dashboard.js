1dd2ad09e054916ad3372f492e383a4ca7086a9d
Jovan Pacheco <jovanjpacheco@gmail.com>
2018-10-25 18:27:50 -0400

    Actualizar listas

--- metrello/staticfiles/scripts/dashboard.js
+++ metrello/staticfiles/scripts/dashboard.js
@@ -34,12 +34,14 @@ usuario_conectado(function(){
 				data-action-name="add_item" aria-label="Add" data-action-id="${ 'uuid' }"> 
 				<span aria-hidden="true">&plus;</span>
 				</button>						
-			    <p class="titulo">${ 'name' }</p>
+			    <p class="titulo" data-action-name="update_list" data-action-id="${ 'uuid' }">${ 'name' }</p>
 			    <p>Prioridad: ${ 'get_priority' }
 			    <span style="float: right;">Tareas: ${ 'nro_tareas' }</span></p>
 			  	</div>`;
+
 				for (var i = 0; i < res.length; i++) {
 					$("#listas").append(lista_template( res[i] ));
+					HDD.setJ(res[i].uuid,res[i]);
 				}
 
 				//eliminar listas
@@ -47,7 +49,6 @@ usuario_conectado(function(){
 					event.preventDefault();
 				    event.stopPropagation();
 				    var uuid = $(event.target).attr('data-action-id');
-
 
 				    _confirma(
 				    "¿Estas seguro de eliminar la lista?",
@@ -66,9 +67,16 @@ usuario_conectado(function(){
 				      });
 
 				    },
-				    function(){swal("Cancelado", "", "info");}
+				    	function(){swal("Cancelado", "", "info");}
 				    )
 
+				});
+
+				// abrir modal crear lista
+				$('#open_modal_lista').on("click",function(event) {
+				    $('#modal_lista').find('[data-action-name="add_list"]').removeClass('hide');
+				    $("#btn_list_update").addClass('hide');					
+					$('#modal_lista').modal('toggle');
 				});
 
 				// crear listas
@@ -100,6 +108,57 @@ usuario_conectado(function(){
 				      });
 				});
 
+				// ver o editar una lista modal
+				$('[data-action-name="update_list"]').on("dblclick",function(event) {
+					event.preventDefault();
+				    event.stopPropagation();
+				    var uuid = $(event.target).attr('data-action-id')
+				    var obj = HDD.getJ(uuid);
+				    HDD.set('lista_seleccionada',uuid)
+				    $("#lista_label_titulo").text('Editar lista');
+				    $("#lista_titulo").val(obj.name);
+
+				    $('#lista_priority option')
+				     .removeAttr('selected')
+				     .filter('[value='+obj.priority+']')
+				     .attr('selected', true).change();
+				    $('#modal_lista').find('[data-action-name="add_list"]').addClass('hide');
+				    $("#btn_list_update").removeClass('hide');
+				    $('#modal_lista').modal('toggle');
+		
+				});
+
+
+				// editar una lista accion
+				$('[data-action-name="update_list_click"]').on("click",function(event) {
+					event.preventDefault();
+				    event.stopPropagation();
+
+					_confirma(
+				    "¿Estas seguro de editar la lista?",
+				    function(){
+
+				    $.ajax({
+				        type: 'PATCH',
+				        url:  '/api/v1/list/'+HDD.get('lista_seleccionada')+'/',  
+				        data: {
+						  "name": $("#lista_titulo").val(),
+						  "priority": $("#lista_priority option:selected").val()
+				        },				         
+				        beforeSend: function (xhr) {
+				            xhr.setRequestHeader("Authorization", 'jwt '+ HDD.get('jwt'));
+				        },
+				        complete: function (Req, textStatus)
+				        {
+				        	_exito_redirect('Lista actualizada','/dashboard/')
+				        }
+				      });
+
+				    },
+				    	function(){swal("Cancelado", "", "info");}
+				    )
+
+				});
 
 				// crear tareas para una lista boton
 				$('[data-action-name="add_item"]').on("click",function(event) {
