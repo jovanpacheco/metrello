00dc93d7d5c152ae9ce7cb3784270d63027d19b6
Jovan Pacheco <jovanjpacheco@gmail.com>
2018-10-25 14:13:36 -0400

    Registro de usuario

--- metrello/staticfiles/scripts/login.js
+++ metrello/staticfiles/scripts/login.js
@@ -1,3 +1,8 @@
+usuario_conectado(function(){
+	location.href = '/dashboard/'
+},
+false);
+
 $("#btn_login").on("click",function(){
     $.ajax({
         type: 'POST',
@@ -31,6 +36,50 @@ $("#btn_login").on("click",function(){
 			if(Req.status == 200)
 			{
 				HDD.set('jwt',res.token);
+				setTimeout(function(){ location.href = '/dashboard/'; }, 50);
+			}
+
+        },//fin complete
+    });
+})
+
+
+$("#btn_registro").on("click",function(){
+    $.ajax({
+        type: 'POST',
+        url:  '/api/users/',
+        data: {
+          "first_name": $("#first_name").val(),
+		  "last_name": $("#last_name").val(),
+		  "email": $("#email").val(),
+		  "password": $("#password1").val(),
+        },
+        complete: function (Req, textStatus)
+        {
+
+        	try{
+				var res = JSON.parse(Req.responseText);            	
+        	}catch(err){
+				console.log('Respuesta no es json u otro : ' + err);
+				console.log(Req.responseText)
+			}
+
+			if(Req.status == 500)
+			{
+				_info('Error en el servidor, contacte a soporte');
+				return;
+			}
+
+			if(Req.status == 400)
+			{
+				_info('No se puede registrar con los datos suministrados');
+				return;
+			}
+
+			if(Req.status == 201)
+			{
+				HDD.set('jwt',res.token);
+				HDD.set('username',res.username);
 				setTimeout(function(){ location.href = '/dashboard/'; }, 50);
 			}
 
